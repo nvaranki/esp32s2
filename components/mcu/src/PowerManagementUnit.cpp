06af7521af204dde2bb7697ab91a2167d3dbf4e5
Nikolai Varankine <nvaranki@tut.by>
2021-03-23 16:34:37 +0300

    PMU state register added

--- components/mcu/src/PowerManagementUnit.cpp
+++ components/mcu/src/PowerManagementUnit.cpp
@@ -5,13 +5,69 @@
 #include "PowerManagementUnit.hpp"
 
 PowerManagementUnit::PowerManagementUnit() :
-    swc( nullptr )
+    swc( nullptr ),
+    status( new SubValueRO( RTC_CNTL_LOW_POWER_ST_REG, RTC_CNTL_MAIN_STATE_M, RTC_CNTL_MAIN_STATE_S) ),
+    idle( new FlagRO( RTC_CNTL_LOW_POWER_ST_REG, RTC_CNTL_MAIN_STATE_IN_IDLE_S ) ),
+    sleep( new FlagRO( RTC_CNTL_LOW_POWER_ST_REG, RTC_CNTL_MAIN_STATE_IN_SLP_S ) ),
+    waitXTAL( new FlagRO( RTC_CNTL_LOW_POWER_ST_REG, RTC_CNTL_MAIN_STATE_IN_WAIT_XTL_S ) ),
+    waitPLL( new FlagRO( RTC_CNTL_LOW_POWER_ST_REG, RTC_CNTL_MAIN_STATE_IN_WAIT_PLL_S ) ),
+    waitO8M( new FlagRO( RTC_CNTL_LOW_POWER_ST_REG, RTC_CNTL_MAIN_STATE_IN_WAIT_8M_S ) ),
+    standby( new FlagRO( RTC_CNTL_LOW_POWER_ST_REG, RTC_CNTL_IN_LOW_POWER_STATE_S ) ),
+    wakeup( new FlagRO( RTC_CNTL_LOW_POWER_ST_REG, RTC_CNTL_IN_WAKEUP_STATE_S ) ),
+    wait( new FlagRO( RTC_CNTL_LOW_POWER_ST_REG, RTC_CNTL_MAIN_STATE_WAIT_END_S ) ),
+    ready( new FlagRO( RTC_CNTL_LOW_POWER_ST_REG, RTC_CNTL_RDY_FOR_WAKEUP_S ) ),
+    pll( new FlagRO( RTC_CNTL_LOW_POWER_ST_REG, RTC_CNTL_MAIN_STATE_PLL_ON_S ) ),
+    xtalISO( new FlagRO( RTC_CNTL_LOW_POWER_ST_REG, RTC_CNTL_MAIN_STATE_XTAL_ISO_S ) ),
+    cocpuDone( new FlagRO( RTC_CNTL_LOW_POWER_ST_REG, RTC_CNTL_COCPU_STATE_DONE_S ) ),
+    cocpuSleep( new FlagRO( RTC_CNTL_LOW_POWER_ST_REG, RTC_CNTL_COCPU_STATE_SLP_S ) ),
+    cocpuWork( new FlagRO( RTC_CNTL_LOW_POWER_ST_REG, RTC_CNTL_COCPU_STATE_SWITCH_S ) ),
+    cocpuStart( new FlagRO( RTC_CNTL_LOW_POWER_ST_REG, RTC_CNTL_COCPU_STATE_START_S ) ),
+    touchDone( new FlagRO( RTC_CNTL_LOW_POWER_ST_REG, RTC_CNTL_TOUCH_STATE_DONE_S ) ),
+    touchSleep( new FlagRO( RTC_CNTL_LOW_POWER_ST_REG, RTC_CNTL_TOUCH_STATE_SLP_S ) ),
+    touchWork( new FlagRO( RTC_CNTL_LOW_POWER_ST_REG, RTC_CNTL_TOUCH_STATE_SWITCH_S ) ),
+    touchStart( new FlagRO( RTC_CNTL_LOW_POWER_ST_REG, RTC_CNTL_TOUCH_STATE_START_S ) ),
+    digiOff( new FlagRO( RTC_CNTL_LOW_POWER_ST_REG, RTC_CNTL_XPD_DIG_S ) ),
+    digiISO( new FlagRO( RTC_CNTL_LOW_POWER_ST_REG, RTC_CNTL_DIG_ISO_S ) ),
+    wifiOff( new FlagRO( RTC_CNTL_LOW_POWER_ST_REG, RTC_CNTL_XPD_WIFI_S ) ),
+    wifiISO( new FlagRO( RTC_CNTL_LOW_POWER_ST_REG, RTC_CNTL_WIFI_ISO_S ) ),
+    periOff( new FlagRO( RTC_CNTL_LOW_POWER_ST_REG, RTC_CNTL_XPD_RTC_PERI_S ) ),
+    periISO( new FlagRO( RTC_CNTL_LOW_POWER_ST_REG, RTC_CNTL_PERI_ISO_S ) ),
+    dcdcOff( new FlagRO( RTC_CNTL_LOW_POWER_ST_REG, RTC_CNTL_XPD_DIG_DCDC_S ) ),
+    rom0Off( new FlagRO( RTC_CNTL_LOW_POWER_ST_REG, RTC_CNTL_XPD_ROM0_S ) )
 {
 }
 
 PowerManagementUnit::~PowerManagementUnit()
 {
-    if( swc != nullptr ) delete swc;
+    if( swc  != nullptr ) delete swc;
+    delete status;
+    delete idle;
+    delete sleep;
+    delete waitXTAL;
+    delete waitPLL;
+    delete waitO8M;
+    delete standby;
+    delete wakeup;
+    delete wait;
+    delete ready;
+    delete pll;
+    delete xtalISO;
+    delete cocpuDone;
+    delete cocpuSleep;
+    delete cocpuWork;
+    delete cocpuStart;
+    delete touchDone;
+    delete touchSleep;
+    delete touchWork;
+    delete touchStart;
+    delete digiOff;
+    delete digiISO;
+    delete wifiOff;
+    delete wifiISO;
+    delete periOff;
+    delete periISO;
+    delete dcdcOff;
+    delete rom0Off;
 }
 
 SleepAndWakeupController* PowerManagementUnit::getSleepAndWakeupController()
--- components/mcu/src/PowerManagementUnit.hpp
+++ components/mcu/src/PowerManagementUnit.hpp
@@ -8,16 +8,52 @@
 #define H_PowerManagementUnit
 
 #include "SleepAndWakeupController.hpp"
+#include "bits/FlagRO.hpp"
+#include "bits/SubValueRO.hpp"
 
 class PowerManagementUnit
 {
 private:
     SleepAndWakeupController* swc;
 public:
+    SubValueRO* const status; //!< rtc main state machine status TODO undocumented
+    FlagRO* const idle; //!< rtc main state machine is in idle state TODO undocumented
+    FlagRO* const sleep; //!< rtc main state machine is in sleep state TODO undocumented
+    FlagRO* const waitXTAL; //!< rtc main state machine is in wait xtal state TODO undocumented
+    FlagRO* const waitPLL; //!< rtc main state machine is in wait pll state TODO undocumented
+    FlagRO* const waitO8M; //!< rtc main state machine is in wait 8m (clock?) state TODO undocumented
+    FlagRO* const standby; //!< rtc main state machine is in the states of low power TODO undocumented
+    FlagRO* const wakeup; //!< rtc main state machine is in the states of wakeup process TODO undocumented
+    FlagRO* const wait; //!< rtc main state machine has been waited for some cycles TODO undocumented
+    FlagRO* const ready; //!< rtc main state machine is ready to receive wake up trigger from wake up source
+    FlagRO* const pll; //!< rtc main state machine is in states that pll should be running TODO undocumented
+    FlagRO* const xtalISO; //!< rtc main state machine: no use any more TODO undocumented
+    FlagRO* const cocpuDone; //!< ulp/cocpu is done TODO undocumented
+    FlagRO* const cocpuSleep; //!< ulp/cocpu is in sleep state TODO undocumented
+    FlagRO* const cocpuWork; //!< ulp/cocpu is about to working. Switch rtc main state TODO undocumented
+    FlagRO* const cocpuStart; //!< ulp/cocpu should start to work TODO undocumented
+    FlagRO* const touchDone; //!< touch is done TODO undocumented
+    FlagRO* const touchSleep; //!< touch is in sleep state TODO undocumented
+    FlagRO* const touchWork; //!< touch is about to working. Switch rtc main state TODO undocumented
+    FlagRO* const touchStart; //!< touch should start to work TODO undocumented
+    FlagRO* const digiOff; //!< digital wrap power down TODO undocumented
+    FlagRO* const digiISO; //!< digital wrap iso TODO undocumented
+    FlagRO* const wifiOff; //!< wifi wrap power down TODO undocumented
+    FlagRO* const wifiISO; //!< wifi iso TODO undocumented
+    FlagRO* const periOff; //!< rtc peripheral power down TODO undocumented
+    FlagRO* const periISO; //!< rtc peripheral iso TODO undocumented
+    FlagRO* const dcdcOff; //!< External DCDC power down TODO undocumented
+    FlagRO* const rom0Off; //!< rom0 power down TODO undocumented
+public:
     PowerManagementUnit();
     virtual ~PowerManagementUnit();
 public:
     SleepAndWakeupController* getSleepAndWakeupController();
+    FastClockRTC* getFastClockRTC();
+    SlowClockRTC* getSlowClockRTC();
+    Oscillator32K* getOscillator32K();
+    Oscillator8M*  getOscillator8M();
+    OscillatorRTC* getOscillatorRTC();
 };
 
 #endif
