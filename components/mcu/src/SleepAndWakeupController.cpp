5d7bb764acf8ec677851601f259d2d7f0e28d52b
Nikolai Varankine <nvaranki@tut.by>
2021-03-27 02:47:52 +0300

    sleep and wakeup control clarified

--- components/mcu/src/SleepAndWakeupController.cpp
+++ components/mcu/src/SleepAndWakeupController.cpp
@@ -5,27 +5,57 @@
 #include "SleepAndWakeupController.hpp"
 
 SleepAndWakeupController::SleepAndWakeupController() :
-    cmd( new BitSetWO( RTC_CNTL_STATE0_REG, 0x00000003 ) ), //TODO mask
-    sts( new BitSetRW( RTC_CNTL_STATE0_REG, 0xE0000000 ) ), //TODO mask
-    we( new BitSetRW( RTC_CNTL_WAKEUP_STATE_REG, RTC_CNTL_WAKEUP_ENA_M ) ),
     wcGPIO( new BitSetRW( RTC_CNTL_EXT_WAKEUP_CONF_REG, RTC_CNTL_EXT_WAKEUP0_LV_M | RTC_CNTL_EXT_WAKEUP1_LV_M ) ),
     sro( new BitSetRW( RTC_CNTL_SLP_REJECT_CONF_REG, RTC_CNTL_DEEP_SLP_REJECT_EN_M | RTC_CNTL_LIGHT_SLP_REJECT_EN_M | RTC_CNTL_SLEEP_REJECT_ENA_M ) ),
     wcEXT1( new BitSetRW( RTC_CNTL_EXT_WAKEUP1_REG, RTC_CNTL_EXT_WAKEUP1_STATUS_CLR_M | RTC_CNTL_EXT_WAKEUP1_SEL_M ) ),
     wsEXT1( new BitSetRO( RTC_CNTL_EXT_WAKEUP1_STATUS_REG, RTC_CNTL_EXT_WAKEUP1_STATUS_M ) ),
-    r2sc( new BitSetRO( RTC_CNTL_SLP_REJECT_CAUSE_REG, RTC_CNTL_REJECT_CAUSE_M ) ),
-    s2wc( new BitSetRO( RTC_CNTL_SLP_WAKEUP_CAUSE_REG, RTC_CNTL_WAKEUP_CAUSE_M ) )
+    interrupt( new FlagWO( RTC_CNTL_STATE0_REG, RTC_CNTL_SW_CPU_INT_S ) )
 {
 }
 
 SleepAndWakeupController::~SleepAndWakeupController()
 {
-    delete cmd;
-    delete sts;
-    delete we;
     delete wcGPIO;
     delete sro;
     delete wcEXT1;
     delete wsEXT1;
-    delete r2sc;
-    delete s2wc;
+    delete interrupt;
+}
+
+SleepAndWakeupController::Sleep::Sleep() :
+    start( new FlagRW( RTC_CNTL_STATE0_REG, RTC_CNTL_SLEEP_EN_S ) )
+{
+}
+
+SleepAndWakeupController::Sleep::~Sleep()
+{
+    delete start;
+}
+
+SleepAndWakeupController::Reject::Reject() :
+    on( new FlagRW( RTC_CNTL_STATE0_REG, RTC_CNTL_SLP_REJECT_S ) ),
+    cause( new BitSetRO( RTC_CNTL_SLP_REJECT_CAUSE_REG, RTC_CNTL_REJECT_CAUSE_M ) ),
+    clear( new FlagWO( RTC_CNTL_STATE0_REG, RTC_CNTL_SLP_REJECT_CAUSE_CLR_S ) )
+{
+}
+
+SleepAndWakeupController::Reject::~Reject()
+{
+    delete on;
+    delete cause;
+    delete clear;
+}
+
+SleepAndWakeupController::Wakeup::Wakeup() :
+    enable( new BitSetRW( RTC_CNTL_WAKEUP_STATE_REG, RTC_CNTL_WAKEUP_ENA_M ) ),
+    on( new FlagRW( RTC_CNTL_STATE0_REG, RTC_CNTL_SLP_WAKEUP_S ) ),
+    cause( new BitSetRO( RTC_CNTL_SLP_WAKEUP_CAUSE_REG, RTC_CNTL_WAKEUP_CAUSE_M ) )
+{
+}
+
+SleepAndWakeupController::Wakeup::~Wakeup()
+{
+    delete enable;
+    delete on;
+    delete cause;
 }
--- components/mcu/src/SleepAndWakeupController.hpp
+++ components/mcu/src/SleepAndWakeupController.hpp
@@ -9,31 +9,20 @@
 #include "soc/rtc_cntl_reg.h"
 #include "bits/BitSetRW.hpp"
 #include "bits/BitSetWO.hpp"
+#include "bits/FlagRW.hpp"
+#include "bits/FlagWO.hpp"
 
 class SleepAndWakeupController
 {
 private:
-    BitSetWO* const cmd; //!< wakeup state control register
-    BitSetRW* const sts; //!< ....Wakeup bitmap enabling register
-    BitSetRW* const we; //!< Wakeup bitmap enabling register
     BitSetRW* const wcGPIO; //!< GPIO wakeup configuration register
     BitSetRW* const sro; //!< Sleep / reject options register
     BitSetRW* const wcEXT1; //!< EXT1 wakeup configuration register
     BitSetRO* const wsEXT1; //!< EXT1 wakeup source register
-    BitSetRO* const r2sc; //!< Reject-to-sleep cause register
-    BitSetRO* const s2wc; //!< Sleep-to-wakeup cause register
 public:
     SleepAndWakeupController();
     virtual ~SleepAndWakeupController();
 public:
-    enum Status : uint32_t
-    {
-        //  RTC_CNTL_STATE0_REG (0x0018)
-        //TODO there are many other bits in API 4.2
-        WAKEUP = RTC_CNTL_SLP_WAKEUP, //!< sleep wakeup
-        REJECT = RTC_CNTL_SLP_REJECT, //!< 0: sleep reject
-        SLEEP  = RTC_CNTL_SLEEP_EN, //!< Sends the chip to sleep
-    };
     enum class WakeupEnable : uint32_t
     {
         // RTC_CNTL_WAKEUP_STATE_REG (0x003C)
@@ -47,15 +36,75 @@ public:
         UART0   = BIT21, //!< Y - - 5
         UART1   = BIT22, //!< Y - - 5
         TOUCH   = BIT23, //!< Y Y - 6
-        //        BIT24,
+        FSM     = BIT24, //!< Y Y - 7 TODO mismatch with doc
         //        BIT25,
-        FSM     = BIT26, //!< Y Y - 7
+        //        BIT26, TODO mismatch with doc
         XTAL32K = BIT27, //!< Y Y Y 8
         RISCV   = BIT28, //!< Y Y - 9
         //        BIT29,
         USB     = BIT30, //!< Y - - 10
         //        BIT31,
     };
+    enum class Peripherals : uint32_t
+    {
+        // RTC_CNTL_SLP_REJECT_CAUSE_REG (0x0124)
+        // RTC_CNTL_SLP_WAKEUP_CAUSE_REG (0x012C)
+        //TODO  (see Table 196) vs WakeupEnable ?!
+        EXT0 = BIT0,  //!< EXT0 GPIO wakeup
+        EXT1 = BIT1,  //!< EXT1 GPIO wakeup
+        GPIO = BIT2,  //!< GPIO wakeup (light sleep only)
+        TIMER = BIT3,  //!< Timer wakeup
+        SDIO  = BIT4,  //!< SDIO wakeup (light sleep only)
+        WIFI  = BIT5,  //!< WIFI wakeup (light sleep only)
+        UART0 = BIT6,  //!< UART0 wakeup (light sleep only)
+        UART1 = BIT7,  //!< UART1 wakeup (light sleep only)
+        TOUCH = BIT8,  //!< Touch wakeup
+        FSM   = BIT9,  //!< FSM ULP wakeup
+        BT    = BIT10, //!< BT wakeup (light sleep only)
+        COCPU = BIT11, //TODO mismatch with doc
+        XTAL32K = BIT12,
+        RISCV   = BIT13,  //!< RISCV ULP wakeup
+        USB     = BIT14,
+    };
+    class Sleep
+    {
+    public:
+        FlagRW*   const start; //!< Sends the chip to sleep
+    public:
+        Sleep();
+        virtual ~Sleep();
+    }
+    const sleep;
+    class Reject
+    {
+    public:
+        FlagRW*   const on; //!< Sleep reject bit
+        BitSetRO* const cause; //!< Reject-to-sleep cause
+        FlagWO*   const clear; //!< Clears the RTC reject-to-sleep cause
+    public:
+        Reject();
+        virtual ~Reject();
+        bool isCause( const Peripherals test ) const { return cause->get( static_cast<uint32_t>( test ) ); };
+    }
+    const reject;
+    class Wakeup
+    {
+    public:
+        BitSetRW* const enable; //!< Wakeup bitmap enabling register
+        FlagRW*   const on; //!< Sleep wakeup bit
+        BitSetRO* const cause; //!< Sleep-to-wakeup cause
+    public:
+        Wakeup();
+        virtual ~Wakeup();
+        bool isEnabled( const WakeupEnable test ) const { return enable->get( static_cast<uint32_t>( test ) ); };
+        void setEnabled( const WakeupEnable mask, bool value ) const { 
+            const_cast<BitSetRW *>(enable)->set( static_cast<uint32_t>( mask ), value ); };
+            //TOTO DEBUG we->set( ((0x1FFFF)<<(15)), value ); };
+        bool isCause( const Peripherals test ) const { return cause->get( static_cast<uint32_t>( test ) ); };
+    }
+    const wakeup;
+    FlagWO* const interrupt; //!< Sends a software RTC interrupt to CPU
+
     enum class WakeupConfigGPIO : uint32_t
     {
         // RTC_CNTL_EXT_WAKEUP_CONF_REG (0x0064)
@@ -83,35 +132,7 @@ public:
         //TODO RTC GPIO @00:22 ?
         //TODO EN0  = BIT0, //!< RTC GPIO wakeup status
     };
-    enum class Peripherals : uint32_t
-    {
-        // RTC_CNTL_SLP_REJECT_CAUSE_REG (0x0124)
-        // RTC_CNTL_SLP_WAKEUP_CAUSE_REG (0x012C)
-        //TODO  (see Table 196) vs WakeupEnable ?!
-        EXT0 = BIT0,  //!< EXT0 GPIO wakeup
-        EXT1 = BIT1,  //!< EXT1 GPIO wakeup
-        GPIO = BIT2,  //!< GPIO wakeup (light sleep only)
-        TIMER = BIT3,  //!< Timer wakeup
-        SDIO  = BIT4,  //!< SDIO wakeup (light sleep only)
-        WIFI  = BIT5,  //!< WIFI wakeup (light sleep only)
-        UART0 = BIT6,  //!< UART0 wakeup (light sleep only)
-        UART1 = BIT7,  //!< UART1 wakeup (light sleep only)
-        TOUCH = BIT8,  //!< Touch wakeup
-        FSM   = BIT9,  //!< FSM ULP wakeup
-        BT    = BIT10, //!< BT wakeup (light sleep only)
-        COCPU = BIT11,
-        XTAL32K = BIT12,
-        RISCV   = BIT13,  //!< RISCV ULP wakeup
-        USB     = BIT14,
-    };
 public:
-    void sendInterruptRTC() { cmd->set( RTC_CNTL_SW_CPU_INT, true ); };
-    void clearCauseR2S() { cmd->set( RTC_CNTL_SLP_REJECT_CAUSE_CLR, true ); };
-    bool getStatus( const Status test ) const { return sts->get( static_cast<uint32_t>( test ) ); };
-    void setStatus( const Status mask, bool value ) { sts->set( static_cast<uint32_t>( mask ), value ); };
-    bool isWakeupEnabled( const WakeupEnable test ) const { return we->get( static_cast<uint32_t>( test ) ); };
-    void setWakeupEnabled( const WakeupEnable mask, bool value ) { we->set( static_cast<uint32_t>( mask ), value ); };
-    uint32_t getWakeupEnabled() const { return we->getAll(); };
     bool getConfigGPIO( const WakeupConfigGPIO test ) const { return wcGPIO->get( static_cast<uint32_t>( test ) ); };
     void setConfigGPIO( const WakeupConfigGPIO mask, bool value ) { wcGPIO->set( static_cast<uint32_t>( mask ), value ); };
     bool getRejectConfig( const RejectConfig test ) const { return sro->get( static_cast<uint32_t>( test ) ); };
@@ -119,9 +140,6 @@ public:
     bool getConfigEXT1( const WakeupConfigEXT1 test ) const { return wcEXT1->get( static_cast<uint32_t>( test ) ); };
     void setConfigEXT1( const WakeupConfigEXT1 mask, bool value ) { wcEXT1->set( static_cast<uint32_t>( mask ), value ); };
     bool isSleepToWakeupCause( const WakeupStatusEXT1 test ) const { return wsEXT1->get( static_cast<uint32_t>( test ) ); };
-    bool isRejectToSleepCause( const Peripherals test ) const { return r2sc->get( static_cast<uint32_t>( test ) ); };
-    bool isSleepToWakeupCause( const Peripherals test ) const { return s2wc->get( static_cast<uint32_t>( test ) ); };
-    uint32_t getSleepToWakeupCause() const { return s2wc->getAll(); };
     //TODO XOR? bool hasAnySleepToWakeupCause() const { return s2wc->get( RTC_CNTL_WAKEUP_CAUSE_M ); };
 };
 
