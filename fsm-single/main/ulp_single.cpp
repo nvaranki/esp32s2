dde02288536fa70d3ea9239b5b84e76c08b8b96f
Nikolai Varankine <nvaranki@tut.by>
2021-04-06 14:46:22 +0300

    improvements and sync with API

--- fsm-single/main/ulp_single.cpp
+++ fsm-single/main/ulp_single.cpp
@@ -1,4 +1,5 @@
-/* ULP FSM Example: manual (software) start of processor.
+/* ULP FSM Example: single manual (software) start of processor with program 
+   loaded at shifted location.
 
    This file contains C code to run assembly program on the ULP. To emplloy C++ 
    API "mcu", the file itself is named as C++ file.
@@ -7,15 +8,30 @@
    To make sure the FSM really runs, an ASM program increments public variable.
    The program is loaded into slow memory with intentional offset.
 
+    Expected output:
+    
+FSM: SLEEP=0 WORK=0 RUNS=0 DONE=1
+FSM: SLEEP=0 WORK=0 RUNS=0 DONE=0
+FSM: SLEEP=0 WORK=0 RUNS=0 DONE=0
+FSM: SLEEP=0 WORK=0 RUNS=0 DONE=0
+FSM: SLEEP=0 WORK=0 RUNS=0 DONE=1
+Edge count from ULP:        533
+Stop FSM clock
+Exit main program
+
    Author © 2021 Nikolai Varankine
 */
 
 #include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 #include "ulp_main.h"
 #include "mcu.h"
 #include "BinaryImageULP.hpp"
+#include "MicroControllerUnit.hpp"
+#include "CoprocessorULP.hpp"
+#include "CoreFSM.hpp"
+#include "PowerManagementUnit.hpp"
+
+void printf( const PowerManagementUnit* pmu );
 
 // ASM program placeholders
 extern const uint8_t ulp_main_bin_start[] asm("_binary_ulp_main_bin_start");
@@ -28,14 +44,11 @@ extern "C"
 void app_main( void )
 {
     MicroControllerUnit* const mcu = new MicroControllerUnit();
+    PowerManagementUnit* const pmu = mcu->getPowerManagementUnit();
     CoprocessorULP* const ulp = mcu->getCoprocessorULP();
     CoreFSM* const fsm = ulp->getCoreFSM();
-    TimerULP* const tmr = ulp->getTimerULP();
     
-    // cancel next timer run if any
-    tmr->active->set( false ); 
-    
-    // load code
+    // load code with intentional offset
     uint32_t offset32 = 128; // 128*4=512=0x00000200
     BinaryImageULP image( ulp_main_bin_start, ulp_main_bin_end - ulp_main_bin_start );
     esp_err_t e = image.loadAt( RTC_SLOW_MEM + offset32 );
@@ -46,24 +59,39 @@ void app_main( void )
         return;
     }
     ulp->entry->set( &ulp_entry - RTC_SLOW_MEM + offset32 );
-    
-    // select FSM to run
-    ulp->setConfig( CoprocessorULP::ConfigCore::CORE, static_cast<bool>( CoprocessorULP::Core::FSM ) );
-    fsm->startOn->set( true );
-    fsm->clockOn->set( true );
-    fsm->clockOff->set( false );
 
     // init shared vars
     ulp_edge_count = 500; // not earlier than the program has been loaded 
+    
+    // select FSM to run
+    fsm->selectForExec();
+    fsm->startOn->on();
+    fsm->clock->on->on();
+    fsm->clock->off->off();
 
     // go!
-    fsm->start->set( false );
-    fsm->start->set( true ); // 0->1 transition matters
-    vTaskDelay( 100 / portTICK_PERIOD_MS ); // single pass run should finish within delay
-    printf("Edge count from ULP: %10d\n", ulp_edge_count);
+    printf( pmu );
+    fsm->start->off();
+    fsm->start->on(); // 0->1 transition matters for software start
+    while( !pmu->cocpuDone->get() )
+        printf( pmu ); // it may stand on long wait, so WORK=0 RUNS=0 is expected
+    printf( pmu );
+    printf( "Edge count from ULP: %10d\n", ulp_edge_count );
 
-    fsm->start->set( false ); // it possibly can be applied right after "true"
-    fsm->clockOn->set( false );
+    // done
+    printf("Stop FSM clock\n");
+    fsm->start->off(); // it possibly can be applied right after "true"
+    fsm->clock->on->off();
     delete mcu;
     printf("Exit main program\n");
+}
+
+void printf( const PowerManagementUnit* pmu )
+{
+    printf( "FSM: SLEEP=%1d WORK=%1d RUNS=%1d DONE=%1d\n",
+        pmu->cocpuSleep->get(),
+        pmu->cocpuWork->get(),
+        pmu->cocpuRuns->get(),
+        pmu->cocpuDone->get()
+        );
 }
