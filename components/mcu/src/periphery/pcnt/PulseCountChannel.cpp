4aed05df71aea63c8b65ae1d02d80eed339754f1
Nikolai Varankine <nvaranki@tut.by>
2021-04-15 17:55:48 +0300

    PulseCountChannel redesigned

--- components/mcu/src/periphery/pcnt/PulseCountChannel.cpp
+++ components/mcu/src/periphery/pcnt/PulseCountChannel.cpp
@@ -1,46 +1,62 @@
 // Input pulse channel.
 //
+// For peripheral input index see Table 22: GPIO Matrix 
+// 39 pcnt_sig_ch0_in0 
+// 40 pcnt_sig_ch1_in0
+// 41 pcnt_ctrl_ch0_in0
+// 42 pcnt_ctrl_ch1_in0
+// ... and so on
+//
 // © 2021 Nikolai Varankine
 
 #include "soc/pcnt_reg.h"
 #include "PulseCountChannel.hpp"
 #include "PulseCountUnit.hpp"
 
-PulseCountChannel::PulseCountChannel( const size_t unit, const size_t i ) :
-    negative( new SubValueRW( PCNT_U0_CONF0_REG + 0xC * unit, 
-        i ? PCNT_CH1_NEG_MODE_U0_M : PCNT_CH0_NEG_MODE_U0_M, 
-        i ? PCNT_CH1_NEG_MODE_U0_S : PCNT_CH0_NEG_MODE_U0_S ) ),
-    positive( new SubValueRW( PCNT_U0_CONF0_REG + 0xC * unit, 
-        i ? PCNT_CH1_POS_MODE_U0_M : PCNT_CH0_POS_MODE_U0_M, 
-        i ? PCNT_CH1_POS_MODE_U0_S : PCNT_CH0_POS_MODE_U0_S ) ),
-    high    ( new SubValueRW( PCNT_U0_CONF0_REG + 0xC * unit, 
-        i ? PCNT_CH1_HCTRL_MODE_U0_M : PCNT_CH0_HCTRL_MODE_U0_M, 
-        i ? PCNT_CH1_HCTRL_MODE_U0_S : PCNT_CH0_HCTRL_MODE_U0_S ) ),
-    low     ( new SubValueRW( PCNT_U0_CONF0_REG + 0xC * unit, 
-        i ? PCNT_CH1_LCTRL_MODE_U0_M : PCNT_CH0_LCTRL_MODE_U0_M, 
-        i ? PCNT_CH1_LCTRL_MODE_U0_S : PCNT_CH0_LCTRL_MODE_U0_S ) ),
+PulseCountChannel::PulseCountChannel( const size_t unit, const size_t channel ) :
+    signal ( unit, channel ),
+    control( unit, channel ),
     unit( unit ),
-    id( i )
+    id( channel )
 {
 }
     
 PulseCountChannel::~PulseCountChannel()
 {
-    delete negative;
-    delete positive;
-    delete high;
-    delete low;
 }
 
-size_t PulseCountChannel::getMatrixIndex( const Input i ) const 
-{ 
-    // See Table 22: GPIO Matrix
-    // 39 pcnt_sig_ch0_in0 
-    // 40 pcnt_sig_ch1_in0
-    // 41 pcnt_ctrl_ch0_in0
-    // 42 pcnt_ctrl_ch1_in0
-    // ... and so on
-    return ( i == Input::SIGNAL ? PCNT_SIG_CH0_IN0_IDX : PCNT_CTRL_CH0_IN0_IDX ) 
-        + unit * ( PulseCountUnit::MAX_CHANNEL * 2 /* item count in Input */ ) 
-        + id; 
+PulseCountChannel::Signal::Signal( const size_t unit, const size_t channel ) :
+    negative( new SubValueRW( PCNT_U0_CONF0_REG + 0xC * unit, 
+        channel ? PCNT_CH1_NEG_MODE_U0_M : PCNT_CH0_NEG_MODE_U0_M, 
+        channel ? PCNT_CH1_NEG_MODE_U0_S : PCNT_CH0_NEG_MODE_U0_S ) ),
+    positive( new SubValueRW( PCNT_U0_CONF0_REG + 0xC * unit, 
+        channel ? PCNT_CH1_POS_MODE_U0_M : PCNT_CH0_POS_MODE_U0_M, 
+        channel ? PCNT_CH1_POS_MODE_U0_S : PCNT_CH0_POS_MODE_U0_S ) ),
+    index( PCNT_SIG_CH0_IN0_IDX + channel
+        + unit * ( PulseCountUnit::MAX_CHANNEL * 2 /* signal + control */ ) )
+{
+}
+    
+PulseCountChannel::Signal::~Signal()
+{
+    delete negative;
+    delete positive;
+}
+
+PulseCountChannel::Control::Control( const size_t unit, const size_t channel ) :
+    high( new SubValueRW( PCNT_U0_CONF0_REG + 0xC * unit, 
+        channel ? PCNT_CH1_HCTRL_MODE_U0_M : PCNT_CH0_HCTRL_MODE_U0_M, 
+        channel ? PCNT_CH1_HCTRL_MODE_U0_S : PCNT_CH0_HCTRL_MODE_U0_S ) ),
+    low ( new SubValueRW( PCNT_U0_CONF0_REG + 0xC * unit, 
+        channel ? PCNT_CH1_LCTRL_MODE_U0_M : PCNT_CH0_LCTRL_MODE_U0_M, 
+        channel ? PCNT_CH1_LCTRL_MODE_U0_S : PCNT_CH0_LCTRL_MODE_U0_S ) ),
+    index( PCNT_CTRL_CH0_IN0_IDX + channel
+        + unit * ( PulseCountUnit::MAX_CHANNEL * 2 /* signal + control */ ) )
+{
+}
+    
+PulseCountChannel::Control::~Control()
+{
+    delete high;
+    delete low;
 }
--- components/mcu/src/periphery/pcnt/PulseCountChannel.hpp
+++ components/mcu/src/periphery/pcnt/PulseCountChannel.hpp
@@ -12,60 +12,78 @@
 
 class PulseCountChannel
 {
-private:
-    /** Action by the counter on detection of negative edge in the input signal */
-    SubValueRW* const negative;
-    /** Action by the counter on detection of positive edge in the input signal */
-    SubValueRW* const positive;
-    /** Modifier to positive/negative action when the control signal is high */
-    SubValueRW* const high;
-    /** Modifier to positive/negative action when the control signal is low */
-    SubValueRW* const low;
 public:
+    /** Input of the channel to count pulses from. */
+    class Signal
+    {
+    private:
+        /** Action by the counter on detection of negative edge in the input signal */
+        SubValueRW* const negative;
+        /** Action by the counter on detection of positive edge in the input signal */
+        SubValueRW* const positive;
+    public:
+        /** Index of the GPIO Matrix input channel */
+        size_t const index;
+    public:
+        Signal( const size_t unit, const size_t channel );
+        virtual ~Signal();
+    public:
+        /** Action by the counter on changed input */
+        enum class Action : uint32_t
+        {
+            /** No effect on counter */
+            IGNORE   = 0,
+            /**  Increase the counter by 1 */
+            INCREASE = 1,
+            /**  Decrease the counter by 1 */
+            DECREASE = 2,
+        };
+        void setOnNegative( const Action action ) { negative->set( static_cast<uint32_t>( action ) ); }
+        void setOnPositive( const Action action ) { positive->set( static_cast<uint32_t>( action ) ); }
+        Action getOnNegative() { return (Action) negative->get(); }
+        Action getOnPositive() { return (Action) positive->get(); }
+    }
+    /*TODO const*/ signal;
+    
+    /** Input of the channel to control counting process on signal input. */
+    class Control
+    {
+    private:
+        /** Modifier to positive/negative action when the control signal is high */
+        SubValueRW* const high;
+        /** Modifier to positive/negative action when the control signal is low */
+        SubValueRW* const low;
+    public:
+        /** Index of the GPIO Matrix input channel */
+        size_t const index;
+    public:
+        Control( const size_t unit, const size_t channel );
+        virtual ~Control();
+    public:
+        /** Modifier to positive/negative action over signal */
+        enum class Action : uint32_t
+        {
+            /** No modification */
+            IGNORE  = 0,
+            /** Invert behavior (increase -> decrease, decrease -> increase) */
+            BOUNCE  = 1,
+            /** Inhibit counter modification */
+            INHIBIT = 2,
+        };
+        void setOnHigh( const Action action ) { high->set( static_cast<uint32_t>( action ) ); }
+        void setOnLow ( const Action action ) { low ->set( static_cast<uint32_t>( action ) ); }
+        Action getOnHigh() { return (Action) high->get(); }
+        Action getOnLow () { return (Action) low ->get(); }
+    }
+    /*TODO const*/ control;
+
+    /** Unit index within controller */
     const size_t unit;
+    /** Channel index within unit */
     const size_t id;
 public:
-    PulseCountChannel( const size_t unit, const size_t i );
+    PulseCountChannel( const size_t unit, const size_t channel );
     virtual ~PulseCountChannel();
-public:
-    /** Action by the counter on changed input */
-    enum class Edge : uint32_t
-    {
-        /** No effect on counter */
-        IGNORE   = 0,
-        /**  Increase the counter by 1 */
-        INCREASE = 1,
-        /**  Decrease the counter by 1 */
-        DECREASE = 2,
-    };
-    /** Modifier to positive/negative action */
-    enum class Extremum : uint32_t
-    {
-        /** No modification */
-        IGNORE  = 0,
-        /** Invert behavior (increase -> decrease, decrease -> increase) */
-        BOUNCE  = 1,
-        /** Inhibit counter modification */
-        INHIBIT = 2,
-    };
-    void setOnNegative( const Edge action ) { negative->set( static_cast<uint32_t>( action ) ); }
-    void setOnPositive( const Edge action ) { positive->set( static_cast<uint32_t>( action ) ); }
-    void setOnHigh( const Extremum action ) { high->set( static_cast<uint32_t>( action ) ); }
-    void setOnLow ( const Extremum action ) { low ->set( static_cast<uint32_t>( action ) ); }
-    Edge getOnNegative() { return (Edge) negative->get(); }
-    Edge getOnPositive() { return (Edge) positive->get(); }
-    Extremum getOnHigh() { return (Extremum) high->get(); }
-    Extremum getOnLow () { return (Extremum) low ->get(); }
-    enum class Input : size_t
-    {
-        SIGNAL  = 0,
-        CONTROL = 1,
-    };
-    /** 
-     * @param i type of channel input
-     * @return index of the GPIO Matrix input channel
-    */
-    size_t getMatrixIndex( const Input i ) const;
 };
 
 #endif
