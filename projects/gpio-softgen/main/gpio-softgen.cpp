ff27cfa9c3905357211379839009364f48725b7e
Nikolai Varankine <nvaranki@tut.by>
2021-05-12 00:58:05 +0300

    enhancements an refactoring

--- projects/gpio-softgen/main/gpio-softgen.cpp
+++ projects/gpio-softgen/main/gpio-softgen.cpp
@@ -2,7 +2,7 @@
 
    The program turns signal on and off for digital output pin.
 
-   !!! PIN_OUT must connect PIN_INP by wire to allow validation.
+   !!! PIN_W must connect PIN_R by wire to allow validation.
 
    Expected output:
 
@@ -36,9 +36,20 @@ Exit Xtensa program
 #include "soc/io_mux_reg.h"
 #include "mcu.h"
 
-#define PIN_OUT 4
-#define PIN_INP 8
-constexpr double FREQ = 40000000.; // 40 MHz
+#ifdef IO_8TO4
+#define PIN_W 8
+#define PIN_R 4
+#define FUNC_W FUNC_GPIO8_GPIO8
+#define FUNC_R FUNC_GPIO4_GPIO4
+#else
+#define PIN_W 4
+#define PIN_R 8
+#define FUNC_W FUNC_GPIO4_GPIO4
+#define FUNC_R FUNC_GPIO8_GPIO8
+#endif
+#define MASK_W BIT(PIN_W)
+#define MASK_R BIT(PIN_R)
+constexpr double FREQ = 40000000.; // 2*40 MHz (two edges per pulse)
 constexpr uint32_t TIME_ATTEMPTS = 3;
 
 void runRegMacro( const uint32_t max, SystemTimer* stm, const uint32_t m4 )
@@ -152,7 +163,7 @@ void runLogged( const uint32_t max, SystemTimer* stm, MatrixBuffer* buf, const u
     printf( " %9.0f Hz\n", FREQ * max / ( t1 - t0 - (t2-t1) ) );
 }
     
-void runChannel( const uint32_t max, SystemTimer* stm, MatrixBuffer::Channel* ch4 )
+void runChannel( const uint32_t max, SystemTimer* stm, MatrixBuffer::Channel* chW )
 {
     printf( "Channel 1 bit..." ); fflush( stdout );
     vTaskDelay( 1 ); //esp_task_wdt_reset();
@@ -160,7 +171,7 @@ void runChannel( const uint32_t max, SystemTimer* stm, MatrixBuffer::Channel* ch
     bool b4 = true;
     for( uint32_t i = 0; i < max; i++ )
     {
-        ch4->write( b4 );
+        chW->write( b4 );
         b4 = !b4;
     }
     uint64_t t1 = stm->getValidValue( TIME_ATTEMPTS );
@@ -168,7 +179,7 @@ void runChannel( const uint32_t max, SystemTimer* stm, MatrixBuffer::Channel* ch
     printf( " %9.0f Hz\n", FREQ * max / ( t1 - t0 - (t2-t1) ) );
 }
     
-void runChWrRead( const uint32_t max, SystemTimer* stm, MatrixBuffer::Channel* ch4, MatrixBuffer::Channel* ch8 )
+void runChWrRead( const uint32_t max, SystemTimer* stm, MatrixBuffer::Channel* chW, MatrixBuffer::Channel* chR )
 {
     printf( "Channel write and read..." ); fflush( stdout );
     vTaskDelay( 1 ); //esp_task_wdt_reset();
@@ -176,8 +187,8 @@ void runChWrRead( const uint32_t max, SystemTimer* stm, MatrixBuffer::Channel* c
     bool b4 = true;
     for( uint32_t i = 0; i < max; i++ )
     {
-        ch4->write( b4 );
-        ch8->read();
+        chW->write( b4 );
+        chR->read();
         b4 = !b4;
     }
     uint64_t t1 = stm->getValidValue( TIME_ATTEMPTS );
@@ -185,7 +196,7 @@ void runChWrRead( const uint32_t max, SystemTimer* stm, MatrixBuffer::Channel* c
     printf( " %9.0f Hz\n", FREQ * max / ( t1 - t0 - (t2-t1) ) );
 }
     
-void runChTested( const uint32_t max, SystemTimer* stm, MatrixBuffer::Channel* ch4, MatrixBuffer::Channel* ch8 )
+void runChTested( const uint32_t max, SystemTimer* stm, MatrixBuffer::Channel* chW, MatrixBuffer::Channel* chR )
 {
     printf( "Test channel write and read..." ); fflush( stdout );
     vTaskDelay( 1 ); //esp_task_wdt_reset();
@@ -193,8 +204,8 @@ void runChTested( const uint32_t max, SystemTimer* stm, MatrixBuffer::Channel* c
     bool b4 = true;
     for( uint32_t i = 0; i < max; i++ )
     {
-        ch4->write( b4 );
-        bool b8 = ch8->read();
+        chW->write( b4 );
+        bool b8 = chR->read();
         if( b4 != b8 )
             printf( "ERROR: %10d v4=%1d v8=%1d\n", i, b4, b8 );
         b4 = !b4;
@@ -204,7 +215,7 @@ void runChTested( const uint32_t max, SystemTimer* stm, MatrixBuffer::Channel* c
     printf( " %9.0f Hz\n", FREQ * max / ( t1 - t0 - (t2-t1) ) );
 }
     
-void runChLogged( const uint32_t max, SystemTimer* stm, MatrixBuffer::Channel* ch4, MatrixBuffer::Channel* ch8 )
+void runChLogged( const uint32_t max, SystemTimer* stm, MatrixBuffer::Channel* chW, MatrixBuffer::Channel* chR )
 {
     printf( "Log channel write and read...\n" ); fflush( stdout );
     vTaskDelay( 1 ); //esp_task_wdt_reset();
@@ -212,8 +223,8 @@ void runChLogged( const uint32_t max, SystemTimer* stm, MatrixBuffer::Channel* c
     bool b4 = true;
     for( uint32_t i = 0; i < max; i++ )
     {
-        ch4->write( b4 );
-        bool b8 = ch8->read();
+        chW->write( b4 );
+        bool b8 = chR->read();
         printf( "\t%10d v4=%1d v8=%1d\n", i, b4, b8 );
         b4 = !b4;
     }
@@ -230,42 +241,49 @@ void app_main( void )
 {
     MicroControllerUnit* const mcu = new MicroControllerUnit();
     ControllerIO* io = mcu->getControllerIO();
-    MatrixBuffer* buf = io->buffer;
-    MatrixBuffer::Channel* ch4 = buf->getChannel( PIN_OUT );
-    MatrixBuffer::Channel* ch8 = buf->getChannel( PIN_INP );
     SystemTimer* stm = mcu->getSystemTimer();
 
-    ExternalPin* p4 = io->getExternalPin( PIN_OUT );
-    p4->input->off();
-    p4->function->set( FUNC_GPIO4_GPIO4 );
-    p4->setPull( ExternalPin::Pull::OPEN );
-    p4->setDriveStrength( ExternalPin::DriveStrength::OUT5MA ); // the more the better form
+    ExternalPin* pW = io->getExternalPin( PIN_W );
+    pW->input->off();
+    pW->function->set( FUNC_W );
+    pW->setPull( ExternalPin::Pull::OPEN );
+    pW->setDriveStrength( ExternalPin::DriveStrength::OUT5MA ); // the more the better form
 
-    ExternalPin* p8 = io->getExternalPin( PIN_INP );
-    p8->input->on();
-    p8->function->set( FUNC_GPIO4_GPIO4 );
-    //TODO ? p8->setPull( ExternalPin::Pull::UP );
+    ExternalPin* pR = io->getExternalPin( PIN_R );
+    pR->input->on();
+    pR->function->set( FUNC_R );
+    //TODO ? pR->setPull( ExternalPin::Pull::UP );
     
     // esp_task_wdt_init( 100000u, false );
     // TaskHandle_t handle;
     // esp_task_wdt_add(handle);
 
-    buf->setEnabled( BIT4 );
-    runRegMacro( 50000000u, stm, BIT4 );            // 8000 kHz
-    runInline32( 30000000u, stm, buf, BIT4 );       // 5450 kHz
-    runSimple  (  8000000u, stm, buf, BIT4 );       // 1210 kHz
-    runWrRead  (  5000000u, stm, buf, BIT4, BIT8 ); //  754 kHz
-    runVerified(  5000000u, stm, buf, BIT4, BIT8 ); //  645 kHz
-    runLogged  (       50u, stm, buf, BIT4, BIT8 ); //  280  Hz
-    buf->setEnabled( 0uL );
+    // Full 32/64 bit buffer I/O
+
+    MatrixBuffer* buf = io->buffer;
+    buf->enableOutput ( MASK_W );
+    buf->disableOutput( MASK_R );
+
+    runRegMacro( 50000000u, stm, MASK_W );              // 8000 kHz
+    runInline32( 30000000u, stm, buf, MASK_W );         // 5450 kHz
+    runSimple  (  8000000u, stm, buf, MASK_W );         // 1210 kHz
+    runWrRead  (  5000000u, stm, buf, MASK_W, MASK_R ); //  754 kHz
+    runVerified(        4u, stm, buf, MASK_W, MASK_R ); //  645 kHz
+    runLogged  (       50u, stm, buf, MASK_W, MASK_R ); //  280  Hz
+    buf->disableOutput( MASK_W ); // to initial state
     
-    ch4->enable( true );
-    ch8->enable( false );
-    runChannel ( 16000000u, stm, ch4 );      // 2700 kHz
-    runChWrRead(  8000000u, stm, ch4, ch8 ); // 1310 kHz
-    runChTested(  8000000u, stm, ch4, ch8 ); // 1190 kHz
-    runChLogged(       80u, stm, ch4, ch8 ); //  270  Hz
-    ch4->enable( false );
+    // 1 bit pipeline I/O
+
+    MatrixBuffer::Channel* chW = buf->getChannel( pW->number );
+    MatrixBuffer::Channel* chR = buf->getChannel( pR->number );
+    chW->output( true  );
+    chR->output( false );
+
+    runChannel ( 16000000u, stm, chW );      // 2700 kHz
+    runChWrRead(  8000000u, stm, chW, chR ); // 1310 kHz
+    runChTested(        4u, stm, chW, chR ); // 1190 kHz
+    runChLogged(       80u, stm, chW, chR ); //  270  Hz
+    chW->output( false ); // to initial state
 
     delete mcu;
     printf( "Exit Xtensa program\n" );
